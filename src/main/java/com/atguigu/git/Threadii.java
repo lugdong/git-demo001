e338e9779c13f83858621479273b335762e5c65b
lugdong <161265935+lugdong@users.noreply.github.com>
2024-03-12 20:11:45 +0800

    Update Threadii.java
    Update Threadii.javavvv

--- src/main/java/com/atguigu/git/Threadii.java
+++ src/main/java/com/atguigu/git/Threadii.java
@@ -4,5 +4,6 @@ public class Threadii {
     public static void main(String[] args) {
         System.out.println("yyyyyy");
         System.out.println("nnnnnnnnnnnnn11");
+         System.out.println("nnnnnnnnnnnnn22222222211");
     }
 }
