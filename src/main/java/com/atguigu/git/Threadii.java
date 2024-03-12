96e2d90381343ed5a6f986fc34c3050b17913809
donglg <2920612809@qq.com>
2024-03-12 19:56:26 +0800

    第1次提交java代码 yyyy

---
+++ src/main/java/com/atguigu/git/Threadii.java
@@ -0,0 +1,8 @@
+package com.atguigu.git;
+
+public class Threadii {
+    public static void main(String[] args) {
+        System.out.println("yyyyyy");
+        System.out.println("nnnnnnnnnnnnn");
+    }
+}
